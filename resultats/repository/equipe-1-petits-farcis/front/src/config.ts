c2be798706ba0082d5f66b1e8f5f569fa3157014
yacinekhacef <yacine.khacef@instant-system.com>
2025-11-14 14:25:43 +0100

    equipe-1: update front journeys url

--- resultats/repository/equipe-1-petits-farcis/front/src/config.ts
+++ resultats/repository/equipe-1-petits-farcis/front/src/config.ts
@@ -2,7 +2,7 @@ const config = {
   useMockData: true, // Set to true to use mock data instead of backend API calls
   backend: {
     journeys: {
-      baseUrl: "https://01f3505f8752.ngrok-free.app/api",
+      baseUrl: "http://localhost:5000/api",
     },
     puzzle: {
       baseUrl: "http://localhost:5000/api/puzzle",
